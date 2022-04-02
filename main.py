6f650635308654dba2956113591b1058c399e450
TanXinJie-SIT <97432075+TanXinJie-SIT@users.noreply.github.com>
2022-04-02 17:57:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Question 2
+print("Hello Glasgow!")
+#Question 1
+print("User Input 1")
+x = input()
+print("User Input 2")
+y = input()
+sum = int(x) + int(y)
+print(sum/2)
+#Question 3
+#Running python 3.10.1
+print("Hello, I am Xin Jie")
+print("Enter module code")
+modCode = input()
+if modCode == "1009":
+    print("Object-Oriented Programming")
+else:
+    print("Invalid")
+
+z = 102
+while(z >= 66):
+    print(z)
+    z -= 1
+
+
+
+
